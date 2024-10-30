4985dc48c93774ecc367da10b82305dfd339d829
jenniffermuima. <jenniffermuima2001@gmail.com>
2024-10-30 16:28:48 -0500

    gladys

--- src/main/java/ec/edu/espoch/practicas/Practicas.java
+++ src/main/java/ec/edu/espoch/practicas/Practicas.java
@@ -12,5 +12,7 @@ public class Practicas {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("hola futuros ingenieros/as");
+        
     }
 }
